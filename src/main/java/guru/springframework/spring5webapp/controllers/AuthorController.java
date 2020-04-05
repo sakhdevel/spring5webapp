e6ab95870d8293afc09e1cd79471f757886dfeaf
a.loginov <a.loginov@sakh.com>
2020-04-05 14:43:19 +1100

    Small change to Task 3

--- src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AuthorController {
 
-    private AuthorRepository authorRepository;
+    private final AuthorRepository authorRepository;
 
     public AuthorController(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
